cdd655c8b0041a72da227e763ad5b71124c3ba73
ovolkov <10935437+rowaasr13@users.noreply.github.com>
2015-06-24 09:52:45 +0300

    v32 docs + toc + changelog

---
+++ ShipyardUI.lua
@@ -0,0 +1,10 @@
+local addon_name, addon_env = ...
+
+-- Confused about mix of CamelCase and_underscores?
+-- Camel case comes from copypasta of how Blizzard calls returns/fields in their code and deriveates
+-- Underscore are my own variables
+
+-- [AUTOLOCAL START]
+-- [AUTOLOCAL END]
+
+addon_env.MissionPage_ButtonsInit("ShipyardMissionPage", GarrisonShipyardFrame.MissionTab.MissionPage)
