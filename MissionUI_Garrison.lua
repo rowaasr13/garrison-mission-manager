94291951be2ed07087cdcbecde8393443571fce0
rowaasr13 <10935437+rowaasr13@users.noreply.github.com>
2016-11-19 07:40:54 +0300

    move "upgrade items" code to prepared placeholder for shared code

--- MissionUI_Garrison.lua
+++ MissionUI_Garrison.lua
@@ -60,102 +60,27 @@ hooksecurefunc("GarrisonMissionButton_SetRewards", function(self, rewards, numRe
    end
 end)
 
-local function SetGameTooltipToItem(self)
-   GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
-   if (self.itemID) then
-      GameTooltip:SetItemByID(self.itemID)
-      return
-   end
-end
-
-local upgrade_items = {
-   { parent = FollowerTab.ItemWeapon, 114128, 114129, 114131, 114616, 114081, 114622, 120302 },
-   { parent = FollowerTab.ItemArmor,  114745, 114808, 114822, 114807, 114806, 114746, 120301 },
-}
-
-local uprade_item_strength = {
-   [114128] = 3,
-   [114129] = 6,
-   [114131] = 9,
-   [114616] = 615,
-   [114081] = 630,
-   [114622] = 645,
-   [114745] = 3,
-   [114808] = 6,
-   [114822] = 9,
-   [114807] = 615,
-   [114806] = 630,
-   [114746] = 645,
-}
-
-local upgrade_item_buttons = {}
-local upgrade_item_normal_textures = setmetatable({}, { __index = function(t, key)
-   local u = upgrade_item_buttons[key]
-   local result = u:GetNormalTexture()
-   return result
-end})
-local upgrade_item_quantity = {}
 local upgrade_buttons_parent = CreateFrame("Frame", nil, FollowerTab.ItemWeapon)
-local function UpdateUpgradeItemStates(frame, followerID, followerInfo)
-   if not followerID then
-      followerID = FollowerTab.followerID
-      if not followerID then return end
-      if not followerInfo then followerInfo = GetFollowerInfo(followerID) end
-   end
 
-   if not followerInfo.isCollected then return end
-   if followerInfo.level < GARRISON_FOLLOWER_MAX_LEVEL then return end
+tinsert(addon_env.upgrade_items, { parent = upgrade_buttons_parent, anchor = FollowerTab.ItemWeapon, 114128, 114129, 114131, 114616, 114081, 114622, 120302 })
+tinsert(addon_env.upgrade_items, { parent = upgrade_buttons_parent, anchor = FollowerTab.ItemArmor,  114745, 114808, 114822, 114807, 114806, 114746, 120301 })
 
-   upgrade_buttons_parent:Show()
-   local _, ilvl_weapon, _, ilvl_armor = GetFollowerItems(followerID)
+local upgrade_item_strength = addon_env.upgrade_item_strength
+upgrade_item_strength[114128] = 3
+upgrade_item_strength[114129] = 6
+upgrade_item_strength[114131] = 9
+upgrade_item_strength[114616] = 615
+upgrade_item_strength[114081] = 630
+upgrade_item_strength[114622] = 645
+upgrade_item_strength[114745] = 3
+upgrade_item_strength[114808] = 6
+upgrade_item_strength[114822] = 9
+upgrade_item_strength[114807] = 615
+upgrade_item_strength[114806] = 630
+upgrade_item_strength[114746] = 645
 
-   for item_type = 1, #upgrade_items do
-      local ilvl_current_type = item_type == 1 and ilvl_weapon or ilvl_armor
-      local item_list = upgrade_items[item_type]
-      local prev = item_list.parent
-      for item_idx = 1, #item_list do
-         local item_id = item_list[item_idx]
-
-         local count = GetItemCount(item_id)
-         if count > 0 then
-            upgrade_item_quantity[item_id]:SetText(count)
-            local texture = upgrade_item_normal_textures[item_id]
-            texture:SetDesaturated(false)
-            texture:SetAlpha(1)
-
-            local strength = uprade_item_strength[item_id]
-            if strength then
-               local action_button_type = "macro"
-               local shift_action_button_type = "macro"
-               if ilvl_current_type == 675 or (strength > 600 and ilvl_current_type >= strength) then
-                  -- Fadeout and disable button if upgrade is absolutely useless
-                  -- i.e. follower has max level or fixed level upgrade is lower than follower current ilevel
-                  texture:SetVertexColor(1, 1, 1)
-                  texture:SetAlpha(0.3)
-                  action_button_type = nil
-                  shift_action_button_type = nil
-               elseif (strength > 3 and strength < 100 and ilvl_current_type + strength > 677) or (strength > 600 and strength - ilvl_current_type < 7) then
-                  -- Dye upgrade yellow and allow only forced use through shift-click if it would be too much waste to use it
-                  -- i.e fixed level upgrade providing less than 7 levels or small upgrades providing less than 4 levels.
-                  texture:SetVertexColor(1, 1, 0)
-                  action_button_type = nil
-               else
-                  texture:SetVertexColor(1, 1, 1)
-               end
-               local button = upgrade_item_buttons[item_id]
-               button:SetAttribute("type", action_button_type)
-               button:SetAttribute("shift-type*", shift_action_button_type)
-            end
-         else
-            upgrade_item_quantity[item_id]:SetText(nil)
-            local texture = upgrade_item_normal_textures[item_id]
-            texture:SetDesaturated(true)
-            texture:SetAlpha(0.3)
-            texture:SetVertexColor(1, 1, 1)
-         end
-      end
-   end
-end
+addon_env.UpgradeItems_InitButtons()
+addon_env.UpgradeItems_InitEvents(GarrisonMissionFrame, upgrade_buttons_parent)
 
 local mechanic_id = {}
 for idx, data in pairs (C_Garrison.GetAllEncounterThreats(LE_FOLLOWER_TYPE_GARRISON_6_0)) do
@@ -172,78 +97,6 @@ local function DrawAbilityCounters(frame, followerID, followerInfo)
       local abilityFrame = self.AbilitiesFrame.Abilities[i]
 
       abilityFrame.Name:SetText(ability.name .. '!')
-   end
-end
-
-hooksecurefunc(GarrisonMissionFrame.FollowerList, "ShowFollower", function(self)
-   local followerID = FollowerTab.followerID
-   if not followerID then return end
-   local followerInfo = GetFollowerInfo(followerID)
-
-   UpdateUpgradeItemStates(self, followerID, followerInfo)
-end)
-
-local function EventToUpdateUpgradeItemStates(self) return UpdateUpgradeItemStates(self) end
-
-event_handlers.BAG_UPDATE_DELAYED = EventToUpdateUpgradeItemStates
-event_handlers.PLAYER_REGEN_ENABLED = EventToUpdateUpgradeItemStates
-event_handlers.PLAYER_REGEN_DISABLED = function()
-   upgrade_buttons_parent:Hide()
-end
-upgrade_buttons_parent:HookScript("OnShow", function(self)
-   EventToUpdateUpgradeItemStates(self)
-   event_frame:RegisterEvent("BAG_UPDATE_DELAYED")
-   event_frame:RegisterEvent("PLAYER_REGEN_ENABLED")
-   event_frame:RegisterEvent("PLAYER_REGEN_DISABLED")
-end)
-upgrade_buttons_parent:HookScript("OnHide", function()
-   event_frame:UnregisterEvent("BAG_UPDATE_DELAYED")
-   event_frame:UnregisterEvent("PLAYER_REGEN_ENABLED")
-   event_frame:UnregisterEvent("PLAYER_REGEN_DISABLED")
-end)
-
-local template_upgrade_button = {
-   "Button",
-   upgrade_buttons_parent,
-   "SecureActionButtonTemplate,SecureHandlerMouseUpDownTemplate",
-   Height = 42, Width = 42,
-   OnEnter = SetGameTooltipToItem,
-   OnLeave = addon_env.HideGameTooltip,
-   -- HighlightTexture = Widget{
-   --    "Texture",
-   --    Texture = "Interface\\BUTTONS\\UI-Common-MouseHilight",
-   --    TexCoord = { 0.15, 0.85, 0.15, 0.85 },
-   --    BlendMode = "ADD",
-   -- },
-}
-
-for item_type = 1, #upgrade_items do
-   local item_list = upgrade_items[item_type]
-   local prev = item_list.parent
-   for item_idx = 1, #item_list do
-      local item_id = item_list[item_idx]
-      template_upgrade_button.TextureToItem = item_id
-      local u = Widget(template_upgrade_button)
-      u.itemID = item_id
-      u:SetPoint("LEFT", prev, "RIGHT", 1, 0)
-
-      local strength = uprade_item_strength[item_id]
-      if strength then
-         -- Widget{"FontString", u, "NumberFontNormal", TOPLEFT = true, Text = strength > 100 and strength or "+" .. strength}
-         local macro_text = "/use item:" .. item_id .."\n/run C_Garrison.CastSpellOnFollower(GarrisonMissionFrame.FollowerTab.followerID)"
-         u:SetAttribute("type", "macro")
-         u:SetAttribute("macrotext", macro_text)
-         u:SetAttribute("shift-type*", "macro")
-         u:SetAttribute("shift-macrotext*", macro_text)
-      else
-         u:SetAttribute("type", "item")
-         u:SetAttribute("item", "item:" .. item_id)
-      end
-
-      upgrade_item_quantity[item_id] = Widget{"FontString", u, "NumberFontNormal", BOTTOMRIGHT = true }
-
-      upgrade_item_buttons[item_id] = u
-      prev = u
    end
 end
 
