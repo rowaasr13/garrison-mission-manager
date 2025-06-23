732ddacc81daf880540be2dc0432e342038b5953
(rowaasr13) Oleg V. Volkov <rowaasr13@gmail.com>
2025-06-23 23:35:05 +0300

    add quest objective location to covenant calling display too

--- MissionUI_Covenant.lua
+++ MissionUI_Covenant.lua
@@ -77,6 +77,17 @@ addon_env.child_frame_cache.CallingExpirationText = addon_env.BuildChildFrameCac
    return expiration
 end)
 
+addon_env.child_frame_cache.CallingObjectiveText = addon_env.BuildChildFrameCache(function(blizzard_covenant_calling_quest)
+   local objective = blizzard_covenant_calling_quest:CreateFontString(nil, "OVERLAY", "GameFontNormalSmall")
+   local attach = addon_env.child_frame_cache.CallingExpirationText[blizzard_covenant_calling_quest]
+   objective:SetPoint("BOTTOM", attach, "TOP", 0, 0)
+   objective:SetJustifyH("CENTER")
+   local fontFile, height, flags = objective:GetFont()
+   objective:SetFont(fontFile, height, (flags or "") .. ',OUTLINE')
+
+   return objective
+end)
+
 local qel_success = {}
 local function RetryQuestTimeLeft(questID, callback)
    -- QuestEventListener alone is not enough, so combine it with .After
@@ -96,12 +107,107 @@ local function RetryQuestTimeLeft(questID, callback)
    return C_Timer.After(0.5, callback)
 end
 
+local quest_locations = {
+   (C_Map.GetAreaInfo(10534)), -- "Bastion"
+   (C_Map.GetAreaInfo(10413)), -- "Revendreth"
+   (C_Map.GetAreaInfo(11510)), -- "Ardenweald"
+   (C_Map.GetAreaInfo(11462)), -- "Maldraxxus"
+   (C_Map.GetAreaInfo(11400)), -- "The Maw"
+   (GetDifficultyInfo(167)),   -- "Torghast", also GroupFinderCategory:113
+}
+
+local coin_of_brokerage = Item:CreateFromItemID(179327)
+local coin_of_brokerage_name = ""
+local rare_resources_quest_name = ""
+local shortests_coin_of_brokerage_location
+coin_of_brokerage:ContinueOnItemLoad(function()
+     coin_of_brokerage_name = coin_of_brokerage:GetItemName()
+end)
+
+local cache_quest_id_to_location = {
+}
+
+local function GetRareResourcesShortestWidthString(questID)
+   if rare_resources_quest_name == nil or rare_resources_quest_name == "" then rare_resources_quest_name = QuestUtils_GetQuestName(questID) end
+   if rare_resources_quest_name == nil or rare_resources_quest_name == "" then return end
+   if coin_of_brokerage_name == nil or coin_of_brokerage_name == "" then return end
+
+   local _, tmp_widget = next(addon_env.child_frame_cache.CallingObjectiveText)
+   local old_text = tmp_widget:GetText()
+
+   tmp_widget:SetText(coin_of_brokerage_name)
+   local width1 = tmp_widget:GetWidth()
+   tmp_widget:SetText(rare_resources_quest_name)
+   local width2 = tmp_widget:GetWidth()
+   tmp_widget:SetText(old_text)
+
+   shortests_coin_of_brokerage_location = (width1 < width2) and coin_of_brokerage_name or rare_resources_quest_name
+   return shortests_coin_of_brokerage_location
+end
+
+local function GetCallingLocation(questID, text, objectiveType, required)
+   -- rare resources
+   if objectiveType == "item" and required == 3 then
+      local location = shortests_coin_of_brokerage_location or GetRareResourcesShortestWidthString(questID) or QuestUtils_GetQuestName(questID) or ""
+      if shortests_coin_of_brokerage_location then cache_quest_id_to_location[questID] = shortests_coin_of_brokerage_location end
+      return location
+   end
+
+   if not text then text = QuestUtils_GetQuestName(questID) end
+   for idx = 1, #quest_locations do
+      local location = quest_locations[idx]
+      if string.match(text, location) then
+         location = quest_locations[idx]
+         cache_quest_id_to_location[questID] = location
+         return location
+      end
+   end
+
+   -- return home location for "complete 3 quests at home" quests and don't cache them
+   if objectiveType == "object" and required == 3 then
+      local covenant_id = C_Covenants.GetActiveCovenantID()
+      if     covenant_id == Enum.CovenantType.Kyrian    then return quest_locations[1]
+      elseif covenant_id == Enum.CovenantType.Venthyr   then return quest_locations[2]
+      elseif covenant_id == Enum.CovenantType.NightFae  then return quest_locations[3]
+      elseif covenant_id == Enum.CovenantType.Necrolord then return quest_locations[4]
+      end
+   end
+end
+
+local function GetCallingObjectiveByQuestID(questID)
+   local text, objectiveType, finished, fulfilled, required = GetQuestObjectiveInfo(questID, 1, false)
+   local location = cache_quest_id_to_location[questID]
+   if not location then
+      location = GetCallingLocation(questID, text, objectiveType, required)
+   else
+   end
+
+   if fulfilled == nil then
+      return location
+   elseif objectiveType == "progressbar" then
+      return GetQuestProgressBarPercent(questID) .. '% ' .. location
+   else
+      return fulfilled .. '/' .. required .. ' ' .. location
+   end
+end
+
 local expiration_text_args = { if_more_than_day_show_only_days = true }
 local function CovenantCallingQuestMixin_GMMHook_Update(self)
    local expiration_text_widget = addon_env.child_frame_cache.CallingExpirationText[self]
+   local objective_text_widget = addon_env.child_frame_cache.CallingObjectiveText[self]
+
    local questID = self.calling.questID
 
-   if not questID then return expiration_text_widget:SetText(nil) end
+   if not questID then
+      objective_text_widget:SetText(nil)
+      return expiration_text_widget:SetText(nil)
+   end
+
+   if C_QuestLog.ReadyForTurnIn(questID) then
+      objective_text_widget:SetText(nil)
+   else
+      objective_text_widget:SetText(GetCallingObjectiveByQuestID(questID))
+   end
 
    local secondsRemaining = C_TaskQuest.GetQuestTimeLeftSeconds(questID)
 
